acc566cfb7a7c30eab4125957875bff02f03931b
Wilson Tian <wilson.tian@perkinelmer.com>
2021-05-04 22:08:25 +0800

    update

--- my-project/src/libs/neo4j-viewer/SVGInline.vue
+++ my-project/src/libs/neo4j-viewer/SVGInline.vue
@@ -1,9 +1,15 @@
+<template>
+  <span v-html="svg" />
+</template>
+
 <script>
 export default {
   name: 'svg-inline',
-  render() {
-    const { svg } = this.$props
-    return svg
-  }
+  props: ['svg']
+  // render() {
+  //   console.log(this.$attrs)
+  //   const { svg } = this
+  //   return svg
+  // }
 }
 </script>
